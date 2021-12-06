473df2c6f6d66e1a80cf3fbfd4f1dbc2db937d32
Benjamin HEBERT <83975236+FastiGun@users.noreply.github.com>
2021-12-06 21:15:25 +0100

    Add files via upload

---
+++ include/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, minimal-ui, user-scalable=no">
+        <title>Exercice</title>
+        <link rel="stylesheet" href="style/style.css"/>
+        <link rel="stylesheet" href="style/bootstrap.min.css"/>
+    </head>
